be5c9e463f7ccf819efbda69734b9f127ed4acda
bcushenberry <cushenberry@gmail.com>
2024-02-10 17:07:45 +0900

    Updated layout and added dark mode

--- chamber/scripts/expandmenu.js
+++ chamber/scripts/expandmenu.js
@@ -5,3 +5,20 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// Add dark mode
+const modeButton = document.querySelector("#dark-mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.checked == true) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+	}
+	
+	else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.background = "#ccc"		
+	}
+});
