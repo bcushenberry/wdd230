d0f028adc9d90288ef16eb97bf61c411a8cfaab5
bcushenberry <cushenberry@gmail.com>
2024-02-25 01:42:40 +0900

    Added the discover.html page for the Chamber website

---
+++ chamber/scripts/visitdate.js
@@ -0,0 +1,28 @@
+const today = Date.now();
+const visitorMessage = document.querySelector(".visitor-message");
+const dayInMs = 84600000;
+
+let numVisits = Number(window.localStorage.getItem("totalVisits")) || 1;
+
+let latestVisit = new Date();
+
+if (numVisits == 1) {
+	visitorMessage.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+else if ((today - latestVisit) < dayInMs) {
+        visitorMessage.textContent = `Back so soon! Awesome!`;
+}
+
+else {
+    let daysSince = (today - latestVisit) / dayInMs;
+    console.log(daysSince);
+    visitorMessage.textContent = `You last visited ${Math.floor(daysSince)} days ago.`;
+}
+
+
+numVisits++;
+latestVisit = today;
+
+localStorage.setItem("totalVisits", numVisits);
+localStorage.setItem("latestVisit", latestVisit);
